2d9f9a55d5adc12fc81ff49b4108195480075cb2
Daniel <gallegoruizdaniel@gmail.com>
2024-02-17 23:04:47 +0100

    Feat - Ordenamos un poco el codigo y l comentamos

--- Test/Test.cpp
+++ Test/Test.cpp
@@ -1,37 +1,71 @@
+#include <iostream>
+//#include <glad/glad.h>
 #include <GLFW/glfw3.h>
+
+GLFWwindow* window;
+/*
+Inicializamos openGL, la libreria glfw y creamos una ventana
+*/
+void inicializar()
+{
+
+	/* Initialize the library */
+	if (!glfwInit()) {
+		std::cout << "> Error al cargar la libreria" << std::endl;
+
+	}
+
+	/* Create a windowed mode window and its OpenGL context */
+	window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+	if (!window)
+	{
+		std::cout << "> Error al iniciar la ventana" << std::endl;
+		glfwTerminate();
+	}
+
+	/* Make the window's context current */
+	glfwMakeContextCurrent(window);
+
+}
+
+/*
+Limpiamos los 2 buffers.
+Front buffer y back buffer.
+El front buffer es el que muestra por pantalla
+El back buffer es el que renderiza.
+El Render hace esto, pinta esto otro y tal, una vez acaba se intercambian.
+El render pasa a ser el buffer que se muestra y el otro pasa a ser el que calucla.
+
+
+Es como si te enseño un dibujo y por detras voy pintando el siguiente.
+*/
+void buffers()
+{
+	/* Render here */
+	glClear(GL_COLOR_BUFFER_BIT);
+
+	/* Swap front and back buffers */
+	glfwSwapBuffers(window);
+
+	/* Poll for and process events */
+	glfwPollEvents();
+
+
+}
 
 int main(void)
 {
-    GLFWwindow* window;
+	inicializar();
 
-    /* Initialize the library */
-    if (!glfwInit())
-        return -1;
+	/* Loop until the user closes the window */
+	while (!glfwWindowShouldClose(window))
+	{
 
-    /* Create a windowed mode window and its OpenGL context */
-    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
-    if (!window)
-    {
-        glfwTerminate();
-        return -1;
-    }
+		buffers();
 
-    /* Make the window's context current */
-    glfwMakeContextCurrent(window);
 
-    /* Loop until the user closes the window */
-    while (!glfwWindowShouldClose(window))
-    {
-        /* Render here */
-        glClear(GL_COLOR_BUFFER_BIT);
+	}
 
-        /* Swap front and back buffers */
-        glfwSwapBuffers(window);
-
-        /* Poll for and process events */
-        glfwPollEvents();
-    }
-
-    glfwTerminate();
-    return 0;
+	glfwTerminate();
+	return 0;
 }
